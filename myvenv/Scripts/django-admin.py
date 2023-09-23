6caed2376a20c09e75e0e848ff854ea2b15687e1
MrJay07 <92586520+MrJay07@users.noreply.github.com>
2023-09-23 19:44:34 +0530

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\mritunjay kumar\Documents\Ecommerce site\django-ecommerce-site\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
